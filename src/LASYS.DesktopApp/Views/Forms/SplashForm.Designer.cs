605c57cedcbef72c7d99651a536581014f619643
ITC - JAYSON OLICIA <oliciajayson@gmail.com>
2025-10-23 16:13:01 +0800

    Update splash screen design and implement custom progress bar

--- src/LASYS.DesktopApp/Views/Forms/SplashForm.Designer.cs
+++ src/LASYS.DesktopApp/Views/Forms/SplashForm.Designer.cs
@@ -28,44 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            progressBar = new ProgressBar();
-            lblStatus = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            pnlLoadingContainer = new Panel();
+            lblCopyright = new Label();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // progressBar
+            // panel1
             // 
-            progressBar.Location = new Point(27, 45);
-            progressBar.Name = "progressBar";
-            progressBar.Size = new Size(331, 13);
-            progressBar.TabIndex = 0;
+            panel1.BackColor = Color.WhiteSmoke;
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(526, 1);
+            panel1.TabIndex = 0;
             // 
-            // lblStatus
+            // panel2
             // 
-            lblStatus.AutoSize = true;
-            lblStatus.Location = new Point(27, 71);
-            lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(12, 15);
-            lblStatus.TabIndex = 1;
-            lblStatus.Text = "-";
+            panel2.BackColor = Color.WhiteSmoke;
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 314);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(526, 1);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.WhiteSmoke;
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 1);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1, 313);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.WhiteSmoke;
+            panel4.Dock = DockStyle.Right;
+            panel4.Location = new Point(525, 1);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1, 313);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.WhiteSmoke;
+            panel5.Controls.Add(pictureBox1);
+            panel5.Controls.Add(label1);
+            panel5.Dock = DockStyle.Top;
+            panel5.Location = new Point(1, 1);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(524, 169);
+            panel5.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.terumo;
+            pictureBox1.Location = new Point(162, 11);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(174, 105);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.SeaGreen;
+            label1.Location = new Point(127, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(274, 30);
+            label1.TabIndex = 6;
+            label1.Text = "LASYS (New Label Design)";
+            // 
+            // pnlLoadingContainer
+            // 
+            pnlLoadingContainer.Location = new Point(59, 180);
+            pnlLoadingContainer.Name = "pnlLoadingContainer";
+            pnlLoadingContainer.Size = new Size(405, 60);
+            pnlLoadingContainer.TabIndex = 5;
+            // 
+            // lblCopyright
+            // 
+            lblCopyright.AutoSize = true;
+            lblCopyright.Location = new Point(25, 267);
+            lblCopyright.Name = "lblCopyright";
+            lblCopyright.Size = new Size(12, 15);
+            lblCopyright.TabIndex = 1;
+            lblCopyright.Text = "-";
             // 
             // SplashForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(393, 109);
-            Controls.Add(lblStatus);
-            Controls.Add(progressBar);
+            BackColor = Color.White;
+            ClientSize = new Size(526, 315);
+            Controls.Add(lblCopyright);
+            Controls.Add(pnlLoadingContainer);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "SplashForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "SplashForm";
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private ProgressBar progressBar;
-        private Label lblStatus;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel pnlLoadingContainer;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label lblCopyright;
     }
 }
--- src/LASYS.DesktopApp/Views/Forms/SplashForm.cs
+++ src/LASYS.DesktopApp/Views/Forms/SplashForm.cs
@@ -1,15 +1,35 @@
 ﻿using LASYS.DesktopApp.Presenters;
 using LASYS.DesktopApp.Views.Interfaces;
+using LASYS.UIControls.Controls;
 
 namespace LASYS.DesktopApp.Views.Forms
 {
     public partial class SplashForm : Form, ISplashView
     {
+        private readonly CustomProgressBar _progressBar;
         private readonly SplashPresenter _presenter;
         public SplashForm(SplashPresenter presenter)
         {
             InitializeComponent();
             _presenter = presenter;
+
+            _progressBar = new CustomProgressBar
+            {
+                Dock = DockStyle.Bottom,
+                ProgressColor = Color.FromArgb(0, 122, 204),
+                ProgressBackgroundColor = Color.LightGray,
+                ShowPercentage = false
+            };
+
+            pnlLoadingContainer.Controls.Add(_progressBar);
+
+            UpdateCopyrightText();
+        }
+
+        private void UpdateCopyrightText()
+        {
+            int year = DateTime.Now.Year;
+            lblCopyright.Text = $"Copyright © {year} Innovathink Corporation.\nAll rights reserved.";
         }
 
         public void CloseView()
@@ -32,9 +52,8 @@ namespace LASYS.DesktopApp.Views.Forms
 
         public void UpdateProgress(int percent, string message)
         {
-            progressBar.Value = percent;
-            lblStatus.Text = message;
-            lblStatus.Refresh();
+            _progressBar.Value = percent;
+            _progressBar.UpdateStatus(message);
         }
 
         public void ShowView() => Application.Run(this);
