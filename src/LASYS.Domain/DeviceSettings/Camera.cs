c7b0c126d873779146a957a0ce057a6eea35190d
ITC - JAYSON OLICIA <oliciajayson@gmail.com>
2026-01-23 12:44:21 +0800

    Added Device Settings model

---
+++ src/LASYS.Domain/DeviceSettings/Camera.cs
@@ -0,0 +1,11 @@
+﻿namespace LASYS.Domain.DeviceSettings
+{
+    public class Camera
+    {
+        public bool Enabled { get; set; } = true;
+        public int CameraId { get; set; } = 0;
+        public int FrameWidth { get; set; } = 3840;
+        public int FrameHeight { get; set; } = 2160;
+        public int FrameRate { get; set; } = 30;
+    }
+}
---
+++ src/LASYS.Domain/DeviceSettings/DeviceConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace LASYS.Domain.DeviceSettings
+{
+    public class DeviceConfiguration
+    {
+        public BarcodeScanner BarcodeScanner { get; set; } = new();
+        public Camera Camera { get; set; } = new();
+    }
+}
