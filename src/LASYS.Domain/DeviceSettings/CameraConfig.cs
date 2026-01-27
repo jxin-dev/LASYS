59763e353283a0924b98f893076e5eefbef27935
ITC - JAYSON OLICIA <oliciajayson@gmail.com>
2026-01-27 09:33:29 +0800

    Update CameraConfig model - changed cameraid to cameraindex - added cameraname property

--- src/LASYS.Domain/DeviceSettings/CameraConfig.cs
+++ src/LASYS.Domain/DeviceSettings/CameraConfig.cs
@@ -3,7 +3,8 @@
     public class CameraConfig
     {
         public bool Enabled { get; set; } = true;
-        public int CameraId { get; set; } = 0;
+        public int CameraIndex { get; set; } = 0;
+        public string CameraName { get; set; } = "Unknown";
         public int FrameWidth { get; set; } = 3840;
         public int FrameHeight { get; set; } = 2160;
         public int FrameRate { get; set; } = 30;
