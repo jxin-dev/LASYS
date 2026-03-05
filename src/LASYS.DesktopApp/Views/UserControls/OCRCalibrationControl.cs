0f6acc593321aa2fbbe77d59a775372e0e9cdab3
ITC - JAYSON OLICIA <oliciajayson@gmail.com>
2026-03-05 17:02:21 +0800

    Add tab for camera settings

--- src/LASYS.DesktopApp/Views/UserControls/OCRCalibrationControl.cs
+++ src/LASYS.DesktopApp/Views/UserControls/OCRCalibrationControl.cs
@@ -1,8 +1,10 @@
-﻿using System.Reflection;
+﻿using System.Drawing.Drawing2D;
+using System.Reflection;
 using LASYS.Application.Contracts;
 using LASYS.DesktopApp.Events;
 using LASYS.DesktopApp.Views.Interfaces;
 using LASYS.UIControls.Controls;
+using OpenCvSharp.Internal.Vectors;
 
 namespace LASYS.DesktopApp.Views.UserControls
 {
@@ -28,6 +30,14 @@ namespace LASYS.DesktopApp.Views.UserControls
         private Label? _lblScannerStatus;
         private Label? _lblPrinterStatus;
         private Label? _lblOcrStatus;
+
+        private CheckBox? _focusCheckBox;
+        private Label? _focusLevelLabel;
+
+        private Panel? _focusOverlay;
+        private TrackBar? _focusSlider;
+        private Label? _focusValueLabel;
+
 
         private Bitmap? _currentFrame;
         public Size PictureBoxSize => picCameraPreview.Size;
@@ -71,6 +81,8 @@ namespace LASYS.DesktopApp.Views.UserControls
             };
 
             pnlContent.Controls.Add(_resizablePanel);
+
+            CameraSettings();
 
             LoadRegisteredItem();
 
@@ -269,7 +281,9 @@ namespace LASYS.DesktopApp.Views.UserControls
             LoadRegisteredOcrItemsRequested?.Invoke(this, EventArgs.Empty);
         }
 
-        private void SystemDevices()
+
+
+        private void CameraSettings()
         {
             var container = new Panel
             {
@@ -277,136 +291,226 @@ namespace LASYS.DesktopApp.Views.UserControls
                 AutoSize = true,
                 Padding = new Padding(15)
             };
-            _resizablePanel.AddTab("System Devices", container);
+
+            _resizablePanel.AddTab("Camera Settings", container);
+
+            var layout = new TableLayoutPanel
+            {
+                Dock = DockStyle.Top,
+                AutoSize = true,
+                ColumnCount = 1,
+                RowCount = 3,
+                Padding = new Padding(0, 5, 0, 0),
+                //CellBorderStyle = TableLayoutPanelCellBorderStyle.Single
+            };
+            container.Controls.Add(layout);
 
             var titleLabel = new Label
             {
-                Text = "System Devices",
+                Text = "Camera Settings",
                 Font = new Font("Segoe UI", 14, FontStyle.Bold),
-                Dock = DockStyle.Top,
+                Dock = DockStyle.Fill,
                 Height = 40,
+                Margin = new Padding(0),
                 TextAlign = ContentAlignment.MiddleLeft
             };
+            layout.Controls.Add(titleLabel, 0, 0);
 
-
-            var table = new TableLayoutPanel
+            var instructionLabel = new Label
             {
-                Dock = DockStyle.Top,
-                AutoSize = true,
-                ColumnCount = 3,
-                RowCount = 4,
-                //CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset
-            };
-
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
-
-            // --- Camera ---
-            var lblCameraTitle = new Label
-            {
-                Text = "Camera:",
-                AutoSize = true,
-                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
-                Padding = new Padding(0, 12, 6, 12),
+                Text = "Select your preferred camera from the list below, choose the desired resolution,\r\nand click 'Save' to store your configuration.\r\n",
+                Font = new Font("Segoe UI", 9, FontStyle.Regular),
                 Dock = DockStyle.Fill,
-                TextAlign = ContentAlignment.MiddleLeft
-            };
-
-            _lblCameraStatus = new Label
-            {
-                Text = "Not connected",
                 AutoSize = true,
-                ForeColor = Color.Gray,
-                Dock = DockStyle.Fill,
                 TextAlign = ContentAlignment.MiddleLeft,
-                Padding = new Padding(0, 12, 0, 12)
+                Margin = new Padding(5, 0, 0, 10)
             };
+            layout.Controls.Add(instructionLabel, 0, 1);
 
-            _btnReconnectCamera = new Button
+            var cameraSettingPanel = new TableLayoutPanel
             {
-                Text = "Reconnect",
+                ColumnCount = 2,
+                RowCount = 4,
                 AutoSize = true,
-                Padding = new Padding(6, 3, 6, 3),
-                Visible = false
+                Dock = DockStyle.Top,
+                //CellBorderStyle = TableLayoutPanelCellBorderStyle.Single
             };
+            cameraSettingPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 140));
+            cameraSettingPanel.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
 
-            _btnReconnectCamera.Click += delegate
+            layout.Controls.Add(cameraSettingPanel, 0, 2);
+
+            var cameraSelectionLabel = new Label
             {
-                SetReconnectCameraButtonVisibility(false);
-                ReconnectCameraRequested?.Invoke(this, EventArgs.Empty);
-            };
-
-
-            // --- Scanner ---
-            var lblScannerTitle = new Label
-            {
-                Text = "Scanner:",
+                Text = "Camera Name:",
                 AutoSize = true,
-                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
-                Padding = new Padding(0, 12, 6, 12),
+                Anchor = AnchorStyles.Left,
+                Margin = new Padding(0, 5, 5, 5)
             };
+            cameraSettingPanel.Controls.Add(cameraSelectionLabel, 0, 0);
 
-            _lblScannerStatus = new Label
+            var cameraSelectionComboBox = new ComboBox
             {
-                Text = "Not detected",
                 AutoSize = true,
-                ForeColor = Color.Gray,
-                Padding = new Padding(0, 12, 0, 12)
+                Width = 250,
+                DropDownStyle = ComboBoxStyle.DropDownList
             };
+            cameraSettingPanel.Controls.Add(cameraSelectionComboBox, 1, 0);
 
-            // --- Printer ---
-            var lblPrinterTitle = new Label
+
+            var cameraResolutionLabel = new Label
             {
-                Text = "Printer:",
+                Text = "Resolution:",
                 AutoSize = true,
-                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
-                Padding = new Padding(0, 12, 6, 12),
+                Anchor = AnchorStyles.Left,
+                Margin = new Padding(0, 5, 5, 5)
             };
+            cameraSettingPanel.Controls.Add(cameraResolutionLabel, 0, 1);
 
-            _lblPrinterStatus = new Label
+            var cameraResolutionComboBox = new ComboBox
             {
-                Text = "Not detected",
                 AutoSize = true,
-                ForeColor = Color.Gray,
-                Padding = new Padding(0, 12, 0, 12)
+                Width = 250,
+                DropDownStyle = ComboBoxStyle.DropDownList
             };
+            cameraSettingPanel.Controls.Add(cameraResolutionComboBox, 1, 1);
 
-            // --- OCR ---
-            var lblOcrTitle = new Label
+            _focusCheckBox = new CheckBox
             {
-                Text = "OCR:",
+                Text = "Manual Focus:",
                 AutoSize = true,
-                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
-                Padding = new Padding(0, 12, 6, 12),
+                Checked = true,
+                CheckAlign = ContentAlignment.MiddleRight,
+                Margin = new Padding(0, 5, 5, 5)
             };
+            cameraSettingPanel.Controls.Add(_focusCheckBox, 0, 2);
 
-            _lblOcrStatus = new Label
+            _focusLevelLabel = new Label
             {
-                Text = "Not ready",
+                Text = "0",
                 AutoSize = true,
-                ForeColor = Color.Gray,
-                Padding = new Padding(0, 12, 0, 12),
+                Font = new Font("Segoe UI", 12, FontStyle.Bold),
+                Margin = new Padding(5, 0, 0, 0)
+            };
+            cameraSettingPanel.Controls.Add(_focusLevelLabel, 1, 2);
+
+            var saveButton = new Button
+            {
+                Text = "Save Configuration",
+                Font = new Font("Segoe UI Semibold,", 9),
+                FlatStyle = FlatStyle.Flat,
+                AutoSize = true,
+                Dock = DockStyle.Fill,
+                Height = 36,
+                ForeColor = Color.White,
+                BackColor = SystemColors.HotTrack,
+                Margin = new Padding(0, 5, 0, 0)
             };
 
-            // Add controls to table
-            table.Controls.Add(lblCameraTitle, 0, 0);
-            table.Controls.Add(_lblCameraStatus, 1, 0);
-            table.Controls.Add(_btnReconnectCamera, 2, 0);
+            cameraSettingPanel.Controls.Add(saveButton, 0, 3);
+
+            InitializeFocusOverlay();
 
 
-            table.Controls.Add(lblScannerTitle, 0, 1);
-            table.Controls.Add(_lblScannerStatus, 1, 1);
+            _focusCheckBox.CheckedChanged += (s, e) =>
+            {
+                _focusOverlay!.Visible = _focusCheckBox.Checked;
+                if (!_focusCheckBox.Checked)
+                    _focusLevelLabel.Text = "0";
+                else
+                    _focusLevelLabel.Text = _focusSlider?.Value.ToString();
 
-            table.Controls.Add(lblPrinterTitle, 0, 2);
-            table.Controls.Add(_lblPrinterStatus, 1, 2);
-
-            table.Controls.Add(lblOcrTitle, 0, 3);
-            table.Controls.Add(_lblOcrStatus, 1, 3);
-
-            container.Controls.Add(table);
-            container.Controls.Add(titleLabel);
+            };
         }
+        private void InitializeFocusOverlay()
+        {
+            // Overlay panel
+            _focusOverlay = new Panel
+            {
+                Width = 280,
+                Height = 44,
+                BackColor = Color.FromArgb(160, 25, 25, 25)
+            };
+
+            _focusOverlay.DoubleBuffered(true);
+
+
+
+            // Slider container
+            Panel sliderContainer = new Panel
+            {
+                Width = 190,
+                Height = 24,
+                Left = 10,
+                Top = 10,
+                BackColor = Color.FromArgb(45, 45, 45),
+            };
+
+            // Trackbar
+            _focusSlider = new TrackBar
+            {
+                Minimum = 0,
+                Maximum = 255,
+                TickStyle = TickStyle.None,
+                Dock = DockStyle.Fill,
+                Value = 120
+            };
+
+          
+            // Value badge
+            _focusValueLabel = new Label
+            {
+                Text = _focusSlider.Value.ToString(),
+                Width = 44,
+                Height = 24,
+                Left = sliderContainer.Right + 10,
+                Top = sliderContainer.Top,
+                TextAlign = ContentAlignment.MiddleCenter,
+                BackColor = Color.FromArgb(45, 45, 45),
+                ForeColor = Color.White,
+                Font = new Font("Segoe UI", 9, FontStyle.Bold)
+            };
+
+            _focusSlider.Scroll += (s, e) =>
+            {
+                _focusValueLabel.Text = _focusSlider.Value.ToString();
+                _focusLevelLabel!.Text = _focusSlider.Value.ToString();
+
+            };
+            sliderContainer.Controls.Add(_focusSlider);
+
+
+            _focusValueLabel.Paint += (s, e) =>
+            {
+                using var pen = new Pen(Color.FromArgb(0, 120, 215));
+                e.Graphics.DrawRectangle(pen, 0, 0,
+                    _focusValueLabel.Width - 1,
+                    _focusValueLabel.Height - 1);
+            };
+
+            // Add controls
+            _focusOverlay.Controls.Add(sliderContainer);
+            _focusOverlay.Controls.Add(_focusValueLabel);
+
+            picCameraPreview.Controls.Add(_focusOverlay);
+
+            CenterOverlay();
+
+            picCameraPreview.Resize += (s, e) => CenterOverlay();
+
+            
+        }
+
+        private void CenterOverlay()
+        {
+            if (_focusOverlay == null) return;
+
+            _focusOverlay.Left =
+                (picCameraPreview.Width - _focusOverlay.Width) / 2;
+
+            _focusOverlay.Top = 10;
+        }
+
         private void CalibrationSetup()
         {
             var container = new Panel
@@ -588,6 +692,144 @@ namespace LASYS.DesktopApp.Views.UserControls
             };
         }
 
+        private void SystemDevices()
+        {
+            var container = new Panel
+            {
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+                Padding = new Padding(15)
+            };
+            _resizablePanel.AddTab("System Devices", container);
+
+            var titleLabel = new Label
+            {
+                Text = "System Devices",
+                Font = new Font("Segoe UI", 14, FontStyle.Bold),
+                Dock = DockStyle.Top,
+                Height = 40,
+                TextAlign = ContentAlignment.MiddleLeft
+            };
+
+
+            var table = new TableLayoutPanel
+            {
+                Dock = DockStyle.Top,
+                AutoSize = true,
+                ColumnCount = 3,
+                RowCount = 4,
+                //CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset
+            };
+
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+
+            // --- Camera ---
+            var lblCameraTitle = new Label
+            {
+                Text = "Camera:",
+                AutoSize = true,
+                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
+                Padding = new Padding(0, 12, 6, 12),
+                Dock = DockStyle.Fill,
+                TextAlign = ContentAlignment.MiddleLeft
+            };
+
+            _lblCameraStatus = new Label
+            {
+                Text = "Not connected",
+                AutoSize = true,
+                ForeColor = Color.Gray,
+                Dock = DockStyle.Fill,
+                TextAlign = ContentAlignment.MiddleLeft,
+                Padding = new Padding(0, 12, 0, 12)
+            };
+
+            _btnReconnectCamera = new Button
+            {
+                Text = "Reconnect",
+                AutoSize = true,
+                Padding = new Padding(6, 3, 6, 3),
+                Visible = false
+            };
+
+            _btnReconnectCamera.Click += delegate
+            {
+                SetReconnectCameraButtonVisibility(false);
+                ReconnectCameraRequested?.Invoke(this, EventArgs.Empty);
+            };
+
+
+            // --- Scanner ---
+            var lblScannerTitle = new Label
+            {
+                Text = "Scanner:",
+                AutoSize = true,
+                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
+                Padding = new Padding(0, 12, 6, 12),
+            };
+
+            _lblScannerStatus = new Label
+            {
+                Text = "Not detected",
+                AutoSize = true,
+                ForeColor = Color.Gray,
+                Padding = new Padding(0, 12, 0, 12)
+            };
+
+            // --- Printer ---
+            var lblPrinterTitle = new Label
+            {
+                Text = "Printer:",
+                AutoSize = true,
+                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
+                Padding = new Padding(0, 12, 6, 12),
+            };
+
+            _lblPrinterStatus = new Label
+            {
+                Text = "Not detected",
+                AutoSize = true,
+                ForeColor = Color.Gray,
+                Padding = new Padding(0, 12, 0, 12)
+            };
+
+            // --- OCR ---
+            var lblOcrTitle = new Label
+            {
+                Text = "OCR:",
+                AutoSize = true,
+                Font = new Font("Segoe UI", 9F, FontStyle.Bold),
+                Padding = new Padding(0, 12, 6, 12),
+            };
+
+            _lblOcrStatus = new Label
+            {
+                Text = "Not ready",
+                AutoSize = true,
+                ForeColor = Color.Gray,
+                Padding = new Padding(0, 12, 0, 12),
+            };
+
+            // Add controls to table
+            table.Controls.Add(lblCameraTitle, 0, 0);
+            table.Controls.Add(_lblCameraStatus, 1, 0);
+            table.Controls.Add(_btnReconnectCamera, 2, 0);
+
+
+            table.Controls.Add(lblScannerTitle, 0, 1);
+            table.Controls.Add(_lblScannerStatus, 1, 1);
+
+            table.Controls.Add(lblPrinterTitle, 0, 2);
+            table.Controls.Add(_lblPrinterStatus, 1, 2);
+
+            table.Controls.Add(lblOcrTitle, 0, 3);
+            table.Controls.Add(_lblOcrStatus, 1, 3);
+
+            container.Controls.Add(table);
+            container.Controls.Add(titleLabel);
+        }
         public void UpdateCoordinateFields(Rectangle imageRegion, Size imageSize)
         {
             _normalizedRegion = null;
@@ -646,8 +888,11 @@ namespace LASYS.DesktopApp.Views.UserControls
 
         public void DisplayFrame(Bitmap bitmap)
         {
-            if (bitmap == null) return;
-
+            if (bitmap == null)
+            {
+                _canDraw = false;
+                return;
+            }
             // Dispose previous frame safely
             if (_currentFrame != null)
             {
