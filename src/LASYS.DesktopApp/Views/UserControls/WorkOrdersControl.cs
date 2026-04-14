5bacbc94b1456ff8021b0e7d4bd795cb85491826
ralph <ralph@gcheck.com>
2026-04-14 15:41:04 +0800

    Work order list to load from database

--- src/LASYS.DesktopApp/Views/UserControls/WorkOrdersControl.cs
+++ src/LASYS.DesktopApp/Views/UserControls/WorkOrdersControl.cs
@@ -7,17 +7,18 @@ namespace LASYS.DesktopApp.Views.UserControls
     public partial class WorkOrdersControl : UserControl, IWorkOrdersView
     {
         public event EventHandler<LabelPrintingRequestedEventArgs>? LabelPrintingRequested;
+        private readonly GridViewWithPagination _gridWithPagination;
         public WorkOrdersControl()
         {
             InitializeComponent();
-            var gridWithPagination = new GridViewWithPagination
+            _gridWithPagination = new GridViewWithPagination
             {
                 PageSize = 5,
                 Dock = DockStyle.Fill,
                 BackColor = Color.White
             };
 
-            gridWithPagination.SetMergedHeaders(
+            _gridWithPagination.SetMergedHeaders(
                 [
                     new HeaderColumn { Text = "Item Code", ColSpan = 1 },
                     new HeaderColumn { Text = "Lot No", ColSpan = 1 },
@@ -39,14 +40,65 @@ namespace LASYS.DesktopApp.Views.UserControls
                 "Qty", "Status"]
             );
 
-            gridWithPagination.SetColumnWidths(
+            _gridWithPagination.SetColumnWidths(
                 100, 100, 100, 100, 100, 100, 100, 100, 100, 50, 100, 50, 100, 50, 100
             );
 
             // Suppose this is from your DB
-            List<SampleData> results = LoadFromDatabase();
+            //List<SampleData> results = LoadFromDatabase();
 
-            gridWithPagination.SetRows(results, item =>
+            //gridWithPagination.SetRows(results, item =>
+            //[
+            //    item.ItemCode,
+            //    item.LotNo,
+            //    item.ExpDate,
+            //    item.PrintType,
+            //    item.Verdict,
+            //    item.DateApproved,
+            //    item.ProductQty.ToString(),
+            //    item.MasterLabelRevNo.ToString(),
+            //    item.LabelInsRevNo.ToString(),
+            //    item.UnitBoxQty.ToString(), item.UnitBoxStatus,
+            //    item.AddtnlUnitBoxQty.ToString(), item.AddtnlUnitBoxStatus,
+            //    item.OuterUnitBoxQty.ToString(), item.OuterUnitBoxStatus
+            //]);
+
+
+
+            _gridWithPagination.RowDoubleClicked += (sender, e) =>
+            {
+                // Handle row double-click event
+                if (e is SampleData data)
+                {
+                    LabelPrintingRequested?.Invoke(this, new LabelPrintingRequestedEventArgs(data.Id));
+
+                }
+
+            };
+
+            pnlContent.Controls.Add(_gridWithPagination);
+
+
+            //List<SampleData> LoadFromDatabase()
+            //{
+            //    // Simulate loading data from a database
+            //    return new List<SampleData>
+            //    {
+            //        new SampleData(1, "SR+OX2051C1", "250529SG", "2030-04-30", "Original", "Approved", "05/29/2025",180000,6,1,"3600","For Printing","","","",""),
+            //        new SampleData(2, "SR+OX2051C2", "250529SG", "2030-04-30", "Original", "Approved", "05/29/2025",180000,6,1,"3600","For Printing","","","",""),
+            //        new SampleData(3, "SR+OX2051C3", "250529SG", "2030-04-30", "Original", "Approved", "05/29/2025",180000,6,1,"3600","For Printing","","","",""),
+            //    };
+            //}
+        }
+
+        public void ShowNotification(string message, string caption, MessageBoxIcon icon)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, icon);
+        }
+
+        public void SetWorkOrders(List<SampleData> workOrders)
+        {
+            _gridWithPagination.SetRows(workOrders, item =>
             [
                 item.ItemCode,
                 item.LotNo,
@@ -61,38 +113,6 @@ namespace LASYS.DesktopApp.Views.UserControls
                 item.AddtnlUnitBoxQty.ToString(), item.AddtnlUnitBoxStatus,
                 item.OuterUnitBoxQty.ToString(), item.OuterUnitBoxStatus
             ]);
-
-
-
-            gridWithPagination.RowDoubleClicked += (sender, e) =>
-            {
-                // Handle row double-click event
-                if (e is SampleData data)
-                {
-                    LabelPrintingRequested?.Invoke(this, new LabelPrintingRequestedEventArgs(data.Id));
-
-                }
-
-            };
-
-            pnlContent.Controls.Add(gridWithPagination);
-
-
-            List<SampleData> LoadFromDatabase()
-            {
-                // Simulate loading data from a database
-                return new List<SampleData>
-                {
-                    new SampleData(1, "SR+OX2051C1", "250529SG", "2030-04-30", "Original", "Approved", "05/29/2025",180000,6,1,"3600","For Printing","","","",""),
-                    new SampleData(2, "SR+OX2051C2", "250529SG", "2030-04-30", "Original", "Approved", "05/29/2025",180000,6,1,"3600","For Printing","","","",""),
-                    new SampleData(3, "SR+OX2051C3", "250529SG", "2030-04-30", "Original", "Approved", "05/29/2025",180000,6,1,"3600","For Printing","","","",""),
-                };
-            }
-        }
-
-        public void ShowNotification(string message, string caption, MessageBoxIcon icon)
-        {
-            MessageBox.Show(message, caption, MessageBoxButtons.OK, icon);
         }
     }
 
