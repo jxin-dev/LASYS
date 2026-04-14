56c1d2252d6494f43813663bffecf7a0cda99a56
ITC - JAYSON OLICIA <oliciajayson@gmail.com>
2026-04-14 08:21:38 +0800

    Add TestLauncher for autologin

---
+++ src/LASYS.TestLauncher/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.btnAutoLogin = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(33, 48)
+        Me.txtUsername.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(148, 24)
+        Me.txtUsername.TabIndex = 0
+        Me.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnAutoLogin
+        '
+        Me.btnAutoLogin.Location = New System.Drawing.Point(33, 80)
+        Me.btnAutoLogin.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnAutoLogin.Name = "btnAutoLogin"
+        Me.btnAutoLogin.Size = New System.Drawing.Size(150, 29)
+        Me.btnAutoLogin.TabIndex = 1
+        Me.btnAutoLogin.Text = "Auto Login"
+        Me.btnAutoLogin.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(72, 26)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 18)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Username"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(217, 143)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnAutoLogin)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Test Launcher"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents btnAutoLogin As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ src/LASYS.TestLauncher/Form1.vb
@@ -0,0 +1,17 @@
+﻿
+Public Class Form1
+    Private Sub btnAutoLogin_Click(sender As Object, e As EventArgs) Handles btnAutoLogin.Click
+
+        Dim appPath As String = "C:\Project\TERUMO\LASYS\src\LASYS.DesktopApp\bin\x86\Debug\net8.0-windows\LASYS.DesktopApp.exe"
+
+        Dim username As String = txtUsername.Text
+
+        Dim psi As New ProcessStartInfo()
+        psi.FileName = appPath
+        psi.Arguments = "--username=" & username
+        psi.WorkingDirectory = IO.Path.GetDirectoryName(appPath)
+
+        Process.Start(psi)
+        Application.Exit()
+    End Sub
+End Class
