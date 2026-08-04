d5887f77d4c937b85a1edb41198df1e757b5a594
ITC - JAYSON OLICIA <oliciajayson@gmail.com>
2026-08-04 15:27:31 +0800

    Refactor validation methods in BatchPrintProcessService

--- src/LASYS.Application/Features/BatchPrinting/Services/BatchPrintProcessService.cs
+++ src/LASYS.Application/Features/BatchPrinting/Services/BatchPrintProcessService.cs
@@ -613,7 +613,7 @@ namespace LASYS.Application.Features.BatchPrinting.Services
             EnsureCanContinue(job);
             NotifyJobStateChanged(job.JobId);
 
-            return StepResult.Success; //comment for real implementation
+            //return StepResult.Success; //comment for real implementation
 
             var isPrinted = await _deviceManager.Printer.IsPrinted(prnFileLocation);
             if (isPrinted)
@@ -662,7 +662,7 @@ namespace LASYS.Application.Features.BatchPrinting.Services
         {
             EnsureCanContinue(job);
 
-            return StepResult.Success; //comment for real implementation
+            //return StepResult.Success; //comment for real implementation
 
             // Ensure scanner is connected
             if (!_deviceManager.Barcode.IsConnected)
